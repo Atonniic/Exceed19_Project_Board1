bd009025e40aa167b0f538ec86ae8e6d32499e8e
Narawit Sariddeechaikul <narawitpun@gmail.com>
2023-02-11 04:14:34 +0700

    remove wifi credentials for privacy and generality

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,8 @@
 
 const int room_id = 0;
 
-const char *ssid = "Pun-iPhone";
-const char *password = "spiderman";
+const char *ssid = "";
+const char *password = "";
 
 const char *ntpServer = "pool.ntp.org";
 const long gmtOffset_sec = 0;
